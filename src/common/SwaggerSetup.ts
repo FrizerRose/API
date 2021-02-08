eb2dee76e714a1b681ceee520d9e46311882afe8
JurajMarkesic <jmarkesic@tvz.hr>
2021-02-08 08:39:46 +0100

    Enabled swagger in production.

--- src/common/SwaggerSetup.ts
+++ src/common/SwaggerSetup.ts
@@ -2,9 +2,9 @@ import { INestApplication } from '@nestjs/common';
 import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
 
 export const setupSwagger = (app: INestApplication): void => {
-  if (process.env.NODE_ENV == 'development') {
-    const options = new DocumentBuilder().setTitle('API documentation').build();
-    const document = SwaggerModule.createDocument(app, options);
-    SwaggerModule.setup(process.env.APP_PREFIX || 'api', app, document);
-  }
+  // if (process.env.NODE_ENV == 'development') {
+  const options = new DocumentBuilder().setTitle('API documentation').build();
+  const document = SwaggerModule.createDocument(app, options);
+  SwaggerModule.setup(process.env.APP_PREFIX || 'api', app, document);
+  // }
 };
