bb8c134d7aee08a4d6224a7b9b4dc882e294426e
JurajMarkesic <jmarkesic@tvz.hr>
2021-02-09 14:50:51 +0100

    Added ssl cert renew script.

---
+++ ssl_renew.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+
+COMPOSE="/usr/local/bin/docker-compose --env-file .env.prod -f docker-compose.yml -f docker-compose.prod.yml --no-ansi"
+DOCKER="/usr/bin/docker"
+
+cd /var/www/API/
+$COMPOSE run certbot renew --dry-run && $COMPOSE kill -s SIGHUP webserver
+$DOCKER system prune -af
