c799a687dbc9e1ef47b71cfddbcc2dcc46c1dc5c
JurajMarkesic <jmarkesic@tvz.hr>
2021-02-10 11:47:28 +0100

    Fixed backup script and removed backup volume.

--- backup_pg.sh
+++ backup_pg.sh
@@ -3,6 +3,5 @@
 COMPOSE="/usr/local/bin/docker-compose --env-file .env.prod -f docker-compose.yml -f docker-compose.prod.yml --no-ansi"
 
 cd /var/www/API/
-$COMPOSE exec postgres_container pg_dumpall -c -U postgres | gzip > /var/lib/postgresql/backup/$(date +"%Y-%m-%d_%H_%M_%S").sql.gz
-
+$COMPOSE exec postgres_container bash -c 'pg_dumpall -c -U postgres' | gzip > ./sql/$(date +"%Y-%m-%d_%H_%M_%S").sql.gz
 
