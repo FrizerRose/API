18f50c1044e371528aecd1b0cb9398b80fc55433
root <jurajmarkesic@gmail.com>
2021-02-10 11:16:37 +0000

    Switched backup script to docker and not compose.

--- backup_pg.sh
+++ backup_pg.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
-COMPOSE="/usr/local/bin/docker-compose --env-file .env.prod -f docker-compose.yml -f docker-compose.prod.yml --no-ansi"
+DOCKER="/usr/bin/docker"
 
-cd /var/www/API/
-$COMPOSE exec postgres_container bash -c 'pg_dumpall -c -U postgres' | gzip > ./sql/$(date +"%Y-%m-%d_%H_%M_%S").sql.gz
+
+$DOCKER exec postgres_container bash -c 'pg_dumpall -c -U postgres' | gzip > /var/www/API/sql/$(date +"%Y-%m-%d_%H_%M_%S").sql.gz
 
